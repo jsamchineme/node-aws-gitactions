dd12b2b8339e8cbff10231a28fa3f00f4b1b1b4d
John Samuel <jsamchineme@gmail.com>
2022-05-24 22:06:17 +0100

    configure env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = 80;
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1 Johnny Sam did it</p>');
