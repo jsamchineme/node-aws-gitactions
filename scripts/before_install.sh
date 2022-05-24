eebb36e0aa0f340c41f320656584fe17e34f129e
John Samuel <jsamchineme@gmail.com>
2022-05-24 21:22:38 +0100

    update scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,10 +6,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesn't exist
-DIR="/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+mkdir -p "/home/ec2-user/express-app"
